e0ca3d60e03f24480b1ec9f875245dc871934f8d
David Shoots <github@karmastics.com>
2026-05-23 20:36:54 -0400

    Only one DATA pin now
    No more multiple-pins for candles.  They're all 'the same string'.
    New HW uses the 2nd data pin.

--- include/hal.h
+++ include/hal.h
@@ -8,7 +8,8 @@
 #define RESET_CONFIG_INPUT 14
 #define RESET_CONFIG_INPUT_MODE INPUT
 #define RESET_CONFIG_INPUT_STATE LOW
-#define CANDLE_LED_PINS {12, 13, 32, 25}
+// #define CANDLE_LED_PINS {12, 13, 32, 25}
+#define CANDLE_LED_PINS {13}
 
 #elif defined ESP32_32E // LCD/Touchscreen
 
@@ -18,7 +19,8 @@
 #define RESET_CONFIG_INPUT 35
 #define RESET_CONFIG_INPUT_MODE INPUT_PULLDOWN
 #define RESET_CONFIG_INPUT_STATE HIGH
-#define CANDLE_LED_PINS {1, 3}
+// #define CANDLE_LED_PINS {1, 3}
+#define CANDLE_LED_PINS {3}
 
 #else // Not a known platform config?
 
